4a5d49486803b2587bf98e30883efa066545dffe
Li Kanhang <664974881@qq.com>
2025-07-04 00:46:27 -0400

    fix

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
-const app = express()
+const postgres = require('postgres');
+const app = express();
 const port = 6000
 
-app.get('/',(req, res) => {
-    res.send('Hello World')
+app.get('/',async (req, res) => {
+    const sql = postgres(`${process.env.DATABASE_URL}`);
+    const response = await sql`SELECT version()`;
+    const { version } =  response[0];
+    res.json({version});
+    res.send('Hello World');
 })
 
 app.listen(port, () => {
